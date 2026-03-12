d3478eb6a35db12817cc9744b554f37831085b7c
maxiggle <clsyfriday@gmail.com>
2026-03-12 16:27:41 +0100

    5th first smart contract

---
+++ SimpleStorage.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.31;
+
+contract SimpleStorage { 
+    uint256  myFavoriteNumber;
+
+
+    struct Person{
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    Person[] public listOfPeople; 
+    
+     function  store(uint256 _favoriteNumber) public {
+        myFavoriteNumber = _favoriteNumber;
+    }
+
+    function addPerson(uint256 _favoriteNumber, string memory name) public  {
+         listOfPeople.push(Person(_favoriteNumber, name));
+    }
+
+    function retrieve( ) public view returns (uint256){
+        return myFavoriteNumber;
+    }
+}
